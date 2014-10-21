719465cfb7f3045ea32cbb7a211c4f3ecdd9ffbd
asissokho <abdoulaye.sissokho@hotmail.fr>
2014-10-21 17:48:38 +0200

    adding configuration elements

---
+++ QMap/Configuration/Parameter.cs
@@ -0,0 +1,18 @@
+﻿using System.Xml.Serialization;
+using System.Data;
+
+namespace QMap.Configuration
+{
+    [XmlType("parameter")]
+    public class Parameter
+    {
+        [XmlAttribute("name")]
+        public string Name { get; set; }
+        [XmlAttribute("dbType")]
+        public DbType ParameterType { get; set; }
+        [XmlAttribute("direction")]
+        public ParameterDirection Direction { get; set; }
+        [XmlAttribute("size")]
+        public int Size { get; set; }
+    }
+}
